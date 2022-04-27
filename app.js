c85a67451d6a6c686ce32d66a14d5640b3f1b42d
German Castro <german@strivelabs.io>
2022-04-27 21:27:38 +0200

    comments unnecesary

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
+// === imports == //
 express = require('express')
 bodyParser = require('body-parser')
 
+
+// === initialisation == //
 app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+// === store == //
 var todoItems = [];
 todoItems.push({ index: 1, value: "learn react", done: false });
 todoItems.push({ index: 2, value: "Go shopping", done: true });
 todoItems.push({ index: 3, value: "buy flowers", done: true });
 var index = 5;
 
+
+// === endpoints == //
+// index endpoint
 app.get('/', (req, res) => res.send('Hello World!'))
 
+// get all tasks
 app.get('/task', (req, res) => {
     return res.json({ data: todoItems, status: "success" })
 })
 
+// create a task
 app.post('/task', (req, res) => {
     todoItems.push({
         index: index++,
@@ -27,15 +36,17 @@ app.post('/task', (req, res) => {
     return res.json({ data: todoItems, status: 'success' })
 })
 
+// delete a task
 app.delete('/task/:id', (req, res) => {
     var todoItems = todoItems.filter(d => d.index != +req.params.id)
     return res.json({ data: todoItems, status: 'success' })
 })
 
+// update a task
 app.patch('/task/:id', (req, res) => {
     todoItems.filter(d => d.index == +req.params.id)[0].done = req.body.done
     return res.json({ data: todoItems, status: 'success' })
 })
 
-
+// === run app == //
 app.listen(8000, () => console.log(`Example app running!`))
